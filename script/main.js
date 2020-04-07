06a10a2e901de79beb9663249078817fc38a079f
Danilo <danilo.maida95@libero.it>
2020-04-07 21:49:20 +0200

    Js- add

--- script/main.js
+++ script/main.js
@@ -1,7 +1,39 @@
 $(document).ready(function(){
 
+var microfone = $('i.microfono');
+var send = $('i.send');
+var input = $('.input-container input');
+var div = $('.chat-container');
+var attivo;
+
+input.click(
+  function () {
+    attivo = 1;{
+    if(attivo == 1)
+    microfone.addClass('hide');
+    send.removeClass('hide')
+    }
+  }
+);
+
+//   $('.right-side').click(
+//     function () {
+//       attivo = 0;
+//       if(attivo == 0){
+//         send.addClass('hide');
+//         microfone.removeClass('hide');
+//       }
+//     }
+// );
 
 
+send.click(
+  function () {
+    console.log(input.val());
+    div.append('<div class = "my-msg">'+input.val()+'</div>');
+    input.val("");
+  }
+);
 
 
 });
