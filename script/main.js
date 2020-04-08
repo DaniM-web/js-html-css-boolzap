58d3a334a5211d8706f029a477fc501c4ad051f2
Danilo <danilo.maida95@libero.it>
2020-04-08 16:09:59 +0200

    TimingFunction - InputSearch

--- script/main.js
+++ script/main.js
@@ -1,39 +1,99 @@
 $(document).ready(function(){
 
-var microfone = $('i.microfono');
-var send = $('i.send');
+var microfone = $('i.fas.fa-microphone');
+var plane = $('i.send');
+var invio = $('span.icon-change');
 var input = $('.input-container input');
 var div = $('.chat-container');
-var attivo;
+var attivo = false;
 
-input.click(
+
+// eventi sull'input della chat
+input.on({
+  // quando il'input è cliccato l'icona cambia
+  'focus':
   function () {
-    attivo = 1;{
-    if(attivo == 1)
-    microfone.addClass('hide');
-    send.removeClass('hide')
+    attivo = true;
+    microfone.hide();
+    plane.show();
+  },
+  //quando il focus esce dall'input l'icona torna microfono
+  'blur':
+  function () {
+    attivo = false;
+    microfone.show();
+    plane.hide();
+  },
+  'keypress':
+  function (e) {
+    if(e.which == 13) {
+      inviaRispondi();
     }
   }
-);
+});
 
-//   $('.right-side').click(
-//     function () {
-//       attivo = 0;
-//       if(attivo == 0){
-//         send.addClass('hide');
-//         microfone.removeClass('hide');
-//       }
-//     }
-// );
+// input.keydown(function(event){
 
+// al click dell'icona paperPlane ivio il messaggio
+invio.on({
+  'click':
+    inviaRispondi
+  // 'keydown':
+  //   press
+});
 
-send.click(
-  function () {
+function inviaRispondi() {
     console.log(input.val());
-    div.append('<div class = "my-msg">'+input.val()+'</div>');
+    //appendo il div con relativa classe
+    div.append('<div class = "my-msg message"> <span>'+ input.val() +'</span><i class="fa fa-chevron-down arrow-msg"></i><span class="message-time">18.01</span></div>');
     input.val("");
-  }
-);
+  // imposto la funzione che risponde al mio msg dopo 1 secondo
+    setTimeout(
+      function(){
+        div.append('<div class = "your-msg message"> <span>Spero di rivederti molto presto</span><i class="fa fa-chevron-down arrow-msg"></i><span class="message-time">18.01</span></div>');
+      }, 1000);
+}
+
+var inputSearch = $('.search-container input');
+var contacts = $('.contact-item')
+
+//eventi sull input di ricerca
+// filtro contatti
+  //gestirte evento su tastiera (oppure su click di bottone di input ricerca)
+  inputSearch.keydown(
+    function (event) {
+      // salvarmi input utente in campo del filtro (stringa1)
+      var textInsert = inputSearch.val().toLowerCase();
+      console.log("Testo inserito: " + textInsert);
+      // selezionare tutti i blocchi di contatto e ciclare tra di essi (each())
+      contacts.each(
+        function () {
+        //salvo in una var il valore del testo del nome nel contatto (stringa2)
+          var nameText = $(this).find('.contact-item-text span.text h5').text().toLowerCase();
+          console.log("NameText: " + nameText);
+          // confronto per vedere se la stringa inserita nell'input è inclusa nel nome del contatto
+          //se l'occorenza è stata trovata lascio il blocco di contatto visibile
+          if(nameText.includes(textInsert)){
+            $(this).show();
+            // altrimenti lo rendo non visibile (this)
+          }else{
+            $(this).hide();
+          }
+        }
+      );
+    }
+  );
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
